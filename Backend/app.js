c0e9bd26e961457d28fc98df0cbbc5b50de5008a
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-03-24 23:09:34 +0530

    cors setup

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ import express from "express";
 import connectToDatabase from './src/config/database.js';
 import authRouter from "./src/routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 const PORT = process.env.PORT;
 const app = express();
@@ -13,6 +14,10 @@ connectToDatabase();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
 app.get('/', (req,res)=>{
     res.send("Welcome to server!!");
