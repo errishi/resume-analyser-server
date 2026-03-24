29f2f5577c715cda02af0f7093cbfc0691efa8b4
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-03-24 18:13:26 +0530

    auth form ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import router from './app.routes.jsx';
+import { RouterProvider } from 'react-router';
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router';
+
+const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const navigate = useNavigate();
+
+  return (
+    <main>
+      <div className='form-container'>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id='username' name='username' placeholder='Enter Username' />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id='email' name='email' placeholder='Enter your email address' />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id='password' name='password' placeholder='Enter password' />
+        </div>
+
+        <button className='button primary-button'>Register</button>
+
+        </form>
+        <p>Already have an account? &nbsp; 
+          <Link to={'/login'}>Login</Link>
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default Register;
